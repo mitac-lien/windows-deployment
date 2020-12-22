10b42d57d375151b621e511f5c7d6ddba0981aba
Chun Hao Lien <57858939+mitac-lien@users.noreply.github.com>
2020-12-22 15:21:46 +0800

    Add camera streaming server scripts

---
+++ cam2ip.ps1
@@ -0,0 +1,25 @@
+﻿# !! Set the camera index manually
+#    I don't find any easy solution to get the camera index to `cv.VideoCapture(index)`
+$cam_id = 0
+
+$cam2ip_url = 'https://github.com/gen2brain/cam2ip/releases/download/1.6/cam2ip-1.6-64bit.zip'
+
+$bin_dir = "$env:USERPROFILE\bin\cam2ip-1.6-64bit"
+if ( -not (Test-Path "$bin_dir") ) {
+    $zip_path = "$env:TMP\cam2ip-1.6-64bit.zip"
+    
+    if (-not (Test-Path "$zip_path")) {
+        Invoke-Webrequest -Uri "$cam2ip_url" -OutFile "$zip_path"
+    }
+    
+    Expand-Archive -LiteralPath "$zip_path" -DestinationPath "$env:USERPROFILE\bin"
+    Remove-item "$zip_path"
+}
+
+$ip_list = Get-NetIPAddress -AddressFamily IPv4
+foreach ($ip in $ip_list) {
+    Write-Output "[$($ip.InterfaceAlias)] `t http://$($ip.IPAddress):56000/mjpeg"
+}
+Write-Output ""
+
+& "$bin_dir\cam2ip.exe" -index "$cam_id" -height 720 -width 1280 -nowebgl -delay 34
---
+++ mjpeg-server.ps1
@@ -0,0 +1,46 @@
+﻿
+$bin_dir = "$env:USERPROFILE\bin"
+$mjpeg_server_bin = "$bin_dir\MJPEGServer.exe"
+$ffmpeg_bin = "$bin_dir\ffmpeg-N-100449-g28aedc7f54-win64-gpl\bin\ffmpeg.exe"
+
+if ( -not (Test-Path "$mjpeg_server_bin") ) {
+    $zip_path = "$env:TMP\mjpeg-server-windows-amd64.zip"
+    
+    if (-not (Test-Path "$zip_path")) {
+        $bin_url = "https://github.com/blueimp/mjpeg-server/releases/download/v1.3.0/mjpeg-server-windows-amd64.zip"
+        Invoke-Webrequest -Uri "$bin_url" -OutFile "$zip_path"
+    }
+    
+    Expand-Archive -LiteralPath "$zip_path" -DestinationPath "$bin_dir"
+    Remove-item "$zip_path"
+}
+
+if ( -not (Test-Path "$ffmpeg_bin") ) {
+    $zip_path = "$env:TMP\ffmpeg-N-100449-g28aedc7f54-win64-gpl.zip"
+    
+    if (-not (Test-Path "$zip_path")) {
+        $bin_url = "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2020-12-21-12-38/ffmpeg-N-100449-g28aedc7f54-win64-gpl.zip"
+        Invoke-Webrequest -Uri "$bin_url" -OutFile "$zip_path"
+    }
+    
+    Expand-Archive -LiteralPath "$zip_path" -DestinationPath "$bin_dir"
+    Remove-item "$zip_path"
+}
+
+# -r fps
+# -q qscale(VBR)
+
+& "$mjpeg_server_bin" -- "$ffmpeg_bin" `
+  -loglevel error `
+  -probesize 32 `
+  -fpsprobesize 0 `
+  -analyzeduration 0 `
+  -fflags nobuffer `
+  -f dshow `
+  -rtbufsize 100M `
+  -r 10 `
+  -video_size 1280x720 `
+  -i video="Logitech HD Webcam C310" `
+  -f mpjpeg `
+  -q 2 `
+  -
